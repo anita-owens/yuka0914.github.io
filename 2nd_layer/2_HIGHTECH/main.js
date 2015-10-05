3de92c9c4c8c26fef13c82df3eadc8a28d2dd274
yuka0914 <s.yuka.takahashi0914@gmail.com>
2015-10-05 16:45:53 +0200

    Layers
    in order to have second layer which is connected to first layer, I put
    new folders!

---
+++ 2nd_layer/2_HIGHTECH/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	console.log("hello world");
+
+	$(".button").click(function() {
+		var maxScroll= "-400vw";
+		// check how many .page children exists to know maximum negative value of margin-left
+		// if we have 4 .page the maximum should be -400vw so do something like 
+		// var maxScroll = number of .page*-100 and set that as a variable
+
+		// ALTERNATIVE
+		// Hide the button left if we are at the leftmost .page
+		// Hide the button right if we are at the rightmost .page
+		// when button left or right is clicked check if currentMargin is 0 or maxMargin (-400) if so hide left or right button
+
+
+  		$(".container").css("margin-left", "-100vw");
+	});
+
+});
